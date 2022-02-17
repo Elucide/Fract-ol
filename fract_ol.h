31cfb78a12ebe24e7e6a8fb364671b349e4c394d
Yoseph SCHECROUN <yschecro@e2r5p12.clusters.42paris.fr>
2022-02-17 19:15:07 +0100

    g

--- fract_ol.h
+++ fract_ol.h
@@ -6,13 +6,16 @@
 /*   By: yschecro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 17:46:34 by yschecro          #+#    #+#             */
-/*   Updated: 2022/02/07 17:59:43 by yschecro         ###   ########.fr       */
+/*   Updated: 2022/02/17 17:49:39 by yschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx_linux/mlx.h"
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+
+#define _C_I 1	
 
 typedef struct complex
 {
@@ -20,4 +23,22 @@ typedef struct complex
 	float	img;
 } complex;
 
+typedef struct size 
+{
+	float	h;
+	float	w;
+} size;
+
+typedef struct mlx
+{
+	void *mlx;
+	void *win;
+} mlx;
+
+#ifndef FRACT_OL_H
+# define FRACT_OL_H
+
 int		main(void);
+
+#endif
+
--- main.c
+++ main.c
@@ -6,43 +6,115 @@
 /*   By: yschecro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 17:52:33 by yschecro          #+#    #+#             */
-/*   Updated: 2022/02/07 17:56:57 by yschecro         ###   ########.fr       */
+/*   Updated: 2022/02/17 19:14:29 by yschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract_ol.h"
 
+float	squared_modulus(complex c)
+{
+	return (sqrt((c.real * c.real) + (c.img * c.img)));
+}
+/*
 void	mandelbrot(int rate, void *mlx, void *mlx_win, int height, int width)
 {
-	int	x;
-	int	y;
-	int n;
-	double _Complex c = 0 + _Complex_I;
-	double _Complex z = 0;
+	int		n = 0;
+	complex	c;
+	complex	z;
+	complex	temp;
 
-	x = 0;
-	y = 0;
-	while (x < width)
+	c.real = -(width / 2);
+	c.img =  -(height / 2);
+	while (c.real < (width / 2))
 	{
-		y = 0;
-		while (y < height)
+		c.img = -((float)height / 2) ;
+		while (c.img < (height / 2) )
 		{
-			while (rate < n)
-			y++;
+			z.real = 0;
+			z.img = 0;
+			n = 0;
+			while (rate > n)
+			{
+				mlx_pixel_put(mlx, mlx_win, (int)c.real + width / 2, (int)c.img + height / 2, 10040319);
+				temp.real = (c.real * c.real) - (c.img * c.img) + z.real;
+				z.img = 2 * c.real * c.img + z.img;
+				z.real = temp.real;
+				if (squared_modulus(z) > 4)
+				{
+					//					printf("squared modulus of z:%f\n", squared_modulus(z));
+					mlx_pixel_put(mlx, mlx_win, (int)c.real + width / 2, (int)c.img + height / 2, 1489139);
+					break ;
+				}
+				n++;
+				//				printf("Im(z) = %f	:	Re(z) = %f\n", z.img, z.real);
+			}
+			c.img++;
 		}
+		c.real++;
 	}
 }
+*/
+void	mandelbrot(int rate, size s, mlx m, complex c)
+{
+	complex	z;
+	complex	temp;
+	int		n;
+	int		done;
+
+	done = 0;
+	z.real = 0;
+	z.img = 0;
+	n = 0;
+	while (n < rate && done == 0)
+	{
+//		mlx_pixel_put(m.mlx, m.win, c.real + s.w / 2, c.img + s.h / 2, 10040319);
+		temp.real = (c.real * c.real) - (c.img * c.img) + z.real;
+		z.img = 2 * c.real * c.img + z.img;
+		z.real = temp.real;
+		if (squared_modulus(z) > 4)
+		{
+			mlx_pixel_put(m.mlx, m.win, c.real + s.w / 2, c.img + s.h / 2, 1489139);
+			done = 1;
+		}
+		n++;
+	}
+	c.img++;
+}
+
+void	screen(mlx m, size s, void(*f)(int, size, mlx, complex))
+{
+	complex	c;
+
+	c.real = -(s.w / 2);
+	c.img =  -(s.h / 2);
+	while (c.real < (s.w / 2))
+	{
+		c.img = -(s.h / 2) ;
+		while (c.img < (s.h / 2) )
+		{
+			f(10, s, m, c);
+			c.img++;
+		}
+		c.real++;
+	}
+}	
+
+void	pouce(size scale, size s, )
+
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	int		x = 200;
-	int		y = 200;
+	mlx		m;
+	size	s;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1000, 2000, "fract-ol");
-	mandelbrot(10, mlx, mlx_win, 1000, 2000);
-	mlx_loop(mlx);
-	return (0);
+	s.h = 720;
+	s.w = 1080;
+	m.mlx = mlx_init();
+	m.win = mlx_new_window(m.mlx, s.w, s.h, "fract-ol");
+//	mandelbrot(10, mlx, mlx_win, height, width);
+	screen(m, s, &mandelbrot);
+	mlx_loop(m.mlx);
+	printf("mandelbrot printed\n");
+	return (free(m.mlx), free(m.win), 0);
 }
